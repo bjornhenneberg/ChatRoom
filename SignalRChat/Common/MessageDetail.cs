38e6e3de90d332d1914f8e6e1b4f5df225a9953c
Bjørn Henneberg <bjorn@henneberg.xyz>
2017-10-04 10:12:07 +0200

    init commit

---
+++ SignalRChat/Common/MessageDetail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRChat.Common
+{
+    public class MessageDetail
+    {
+
+        public string UserName { get; set; }
+        public string Message { get; set; }
+        public DateTime? TimeStamp { get; set; }
+    }
+}
